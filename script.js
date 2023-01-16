50cd97c3be6f27691f14bee29db770b8f4369838
unknown <Vegas2813@gmail.com>
2023-01-16 20:44:28 +0200

    index.html

--- script.js
+++ script.js
@@ -1,136 +1,89 @@
-  
-   //Computer choice
-   
-   function getComputerChoice() {
-      const random = (Math.floor(Math.random() * 3));
-    
-      switch (random) {
-        
-        case 0:
-         return('rock')
-          break;
-        case 1:
-          return('paper')
-          break;
-      
-          case 2:
-            return ('scissors')
-          break;
-      
-        default:
-      }
-    
-    
+
+
+//do game through listeners
+const button = document.querySelectorAll ('.button');
+
+button.forEach((button) => {
+  button.addEventListener('click', function () {
+  const pInput = this.textContent;
+  const cOptions = ['rock', 'paper', 'scissors'];
+  const cInput = cOptions[Math.floor(Math.random() * 3)];
+
+  compareInputs(pInput, cInput);
+  updateScore();
+
+  if (checkWinner()) {
+    pScore = cScore = 0;
+    updateScore();
+  }
+
+});
+});
+
+
+function compareInputs(pInput, cInput) {
+  const currentMatch = `${pInput} vs ${cInput}`;
+
+  // Tie check
+  if (pInput === cInput) {
+    alert(`${currentMatch} is a Tie`);
+    return;
+  }
+
+  // Rock
+  if (pInput === 'rock') {
+    if (cInput === 'scissors') {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
     }
-
-
-   
-//Specifies round win/game win messages
-let playerWinRound = "Player wins this round!"
-let computerWinRound = "Computer wins this round!"
-let draw = "Draw!"
-let playerWin = "Player wins the game! Congratulations!"
-let computerWin = "Computer wins the game! Congratulations!"
-
- //Plays one round of RPS
-    function playSingleRound(playerSelection, computerSelection) {
-     
-      if (computerSelection == playerSelection) {
-        return draw;
-      
-      } else if (computerSelection == 'paper' && playerSelection === 'rock') {
-        return computerWinRound;
-      
-      } else if (computerSelection == 'paper' && playerSelection === 'scissors') {
-        return playerWinRound;
-      
-      } else if (computerSelection == 'scissors' && playerSelection === 'rock') {
-        return playerWinRound;
-      
-      } else if (computerSelection == 'scissors' && playerSelection === 'paper') {
-        return computerWinRound;
-      
-      } else if (computerSelection == 'rock' && playerSelection === 'scissors') {
-        return computerWinRound;
-      
-      } else if (computerSelection == 'rock' && playerSelection === 'paper') {
-        return playerWinRound;
-      
-      
-      }else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
-      }
-    
-      }
-
-  
-    
-      const playerSelection = prompt('Please enter one of the following simbols (Rock, Paper or Scissors): ').toLowerCase();
-      const computerSelection = getComputerChoice();
-      let playerScore = 0;
-      let computerScore = 0;
-      let draws = 0;
-      
-    
-// Full game of 5 rounds
-   function game () {
-
-    for (let i = 0; i < 5; i++) {
-      
-        const playerSelection = prompt('Please enter one of the following simbols (Rock, Paper or Scissors): ').toLowerCase();
-        const computerSelection = getComputerChoice();
-        let roundResult = playSingleRound(playerSelection, computerSelection);
-  console.log(roundResult);
-  gameScore(roundResult);
-  console.log("Your score is " + playerScore);
-    console.log("The computer's score is " + computerScore);
-   
-    }     
-  
-}
-
-   game ();
-
-
-//Keeps score prints out correct messages based on score
-
-let result = playSingleRound ();
-
-function gameScore (result) {
-
-
-  if (result === playerWinRound) {
-(playerScore++);
-
-  }else if (result === computerWinRound ) {
-     (computerScore++);
-
-}else if (result === draw) {
-   draws++;
-  
-}else {
-  (draws++);
-
-}
-
-}
-
-
-gameScore (result);
-
-// Prints out correct messages based on score
-function showWinner () {
-
-  if (playerScore > computerScore) {
-    return playerWin;
-  
-  }else if (computerScore > playerScore) {
-      return computerWin;
-  
-  }else {
-    return draw;
+  }
+  // Paper
+  else if (pInput === 'paper') {
+    if (cInput === 'rock') {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
+  }
+  // Scissors
+  else if (pInput === 'scissors') {
+    if (cInput === 'paper') {
+      alert(`${currentMatch} = You Win`);
+      pScore++;
+    } else {
+      alert(`${currentMatch} = Computer Wins`);
+      cScore++;
+    }
   }
 }
 
-showWinner ();
-console.log (showWinner ());
+//Score
+
+let pScore = 0;
+let cScore = 0;
+
+//updateScore
+function updateScore() {
+  document.getElementById("p-score").textContent = pScore;
+  document.getElementById("c-score").textContent = cScore;
+}
+
+
+
+function checkWinner() {
+  if (pScore === 5 || cScore === 5) {
+    const winner =
+      pScore === 5
+        ? "You win the game! Congratulations!"
+        : "Computer wins the game! Try again next time!";
+    alert(winner);
+    return true;
+  }
+  return false;
+}
+
